6507ab387df2200c5a7625eaadcf5e1602223953
joekennerly <joseph.kennerly@gmail.com>
2019-11-20 14:33:59 -0600

    initial commit

---
+++ src/scripts/main.js
@@ -0,0 +1,7 @@
+// 11/20/2019 -------------------------------------------------------
+/*
+Write a function that always returns 5
+Sounds easy right? Just bear in mind that you can't use any of the
+following characters: 0123456789*+-/
+*/
+const unusualFive = () => ['a', 'b', 'c', 'd', 'e'].length
